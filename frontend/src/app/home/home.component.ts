37ef97d5b1bd0df52cc247a16282dc213970c40b
Divyamal <divyamohan.kp@gmail.com>
2025-11-16 01:03:37 +0530

    add to cart in home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { Product } from '../models/product.model';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,18 @@ export class HomeComponent {
   currentYear = new Date().getFullYear();
   products: Product[] = [];
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.apiService.getProducts().subscribe(data => {
       this.products = data;
     });
+  }
+
+  addToCart(product: Product) {
+    if (product) {
+      this.cartService.addToCart(product, 1);
+      alert("Added to cart!");
+    }
   }
 }
